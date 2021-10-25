e981f71f13006e96e6ec6f2c340138803ddd426d
Rivers Yang <rivers.yang@icloud.com>
2021-10-25 23:42:45 +0800

    Correct default value of `minimum_total_stake_price_for_booting` in `ProtocolSettings`.

--- appchain-anchor/src/settings_manager.rs
+++ appchain-anchor/src/settings_manager.rs
@@ -7,7 +7,7 @@ impl Default for ProtocolSettings {
             minimum_validator_deposit: U128::from(10_000 * OCT_DECIMALS_VALUE),
             minimum_delegator_deposit: U128::from(1000 * OCT_DECIMALS_VALUE),
             minimum_total_stake_price_for_booting: U128::from(
-                500_000 * USD_DECIMALS_VALUE * OCT_DECIMALS_VALUE,
+                500_000 * USD_DECIMALS_VALUE,
             ),
             maximum_market_value_percent_of_near_fungible_tokens: 33,
             maximum_market_value_percent_of_wrapped_appchain_token: 67,
