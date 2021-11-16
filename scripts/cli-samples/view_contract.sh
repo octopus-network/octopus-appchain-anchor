2905c412e820713f9b88468ee04f25d17f7282f4
Rivers Yang <rivers.yang@icloud.com>
2021-11-16 23:04:46 +0800

    Add near cli sample scripts.

---
+++ scripts/cli-samples/view_contract.sh
@@ -0,0 +1,17 @@
+#
+export NEAR_ENV=$1
+#
+#
+#
+near view $2 get_anchor_settings ''
+near view $2 get_appchain_settings ''
+near view $2 get_protocol_settings ''
+near view $2 get_oct_token ''
+near view $2 get_wrapped_appchain_token ''
+near view $2 get_near_fungible_tokens ''
+near view $2 get_appchain_state ''
+near view $2 get_anchor_status ''
+near view $2 get_storage_balance ''
+near view $2 get_index_range_of_staking_history ''
+near view $2 get_staking_history '{"index":"0"}'
+near view $2 get_validator_list_of '{"era_number":null}'
