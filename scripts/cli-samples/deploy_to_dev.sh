6660f9ffa8a1e31e4c590fbfe04857b43e968d33
Rivers Yang <rivers.yang@icloud.com>
2021-11-25 15:53:07 +0800

    Correct implementation of storage migration.

--- scripts/cli-samples/deploy_to_dev.sh
+++ scripts/cli-samples/deploy_to_dev.sh
@@ -21,3 +21,10 @@ near deploy --accountId tchain.dev-oct-registry.testnet --wasmFile res/appchain_
 near call tchain.dev-oct-registry.testnet new '{"appchain_id":"tchain","appchain_registry":"dev-oct-registry.testnet","oct_token":"oct.beta_oct_relay.testnet"}' --accountId dev-oct-registry.testnet --gas 200000000000000
 near call oct.beta_oct_relay.testnet storage_deposit '{"account_id":"tchain.dev-oct-registry.testnet","registration_only":null}' --accountId dev-oct-registry.testnet --deposit 1
 near call tchain.dev-oct-registry.testnet migrate_state '' --accountId dev-oct-registry.testnet --gas 200000000000000
+#
+#
+#
+near dev-deploy --wasmFile res/old_appchain_anchor.wasm
+near call dev-1637826424729-44236713958396 new '{"appchain_id":"debio-dev-1","appchain_registry":"dev-oct-registry.testnet","oct_token":"oct.beta_oct_relay.testnet"}' --accountId dev-1637826424729-44236713958396 --gas 200000000000000
+near deploy --accountId dev-1637826424729-44236713958396 --wasmFile res/appchain_anchor.wasm
+near call dev-1637826424729-44236713958396 migrate_state '' --accountId dev-1637826424729-44236713958396 --gas 200000000000000
