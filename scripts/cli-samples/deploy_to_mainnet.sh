cdf0a831068d1c042462b071a0f1f60c8975ac91
Rivers Yang <rivers.yang@icloud.com>
2021-12-02 23:09:49 +0800

    Update deploy_to_mainnet.sh

--- scripts/cli-samples/deploy_to_mainnet.sh
+++ scripts/cli-samples/deploy_to_mainnet.sh
@@ -12,3 +12,4 @@ near call debionetwork.octopus-registry.near migrate_state '' --accountId octopu
 near call debionetwork.octopus-registry.near set_token_price_maintainer_account '{"account_id":"octopus-registry.near"}' --accountId octopus-registry.near
 near call debionetwork.octopus-registry.near set_price_of_oct_token '{"price":"2740000"}' --accountId octopus-registry.near
 #
+near call debionetwork.octopus-registry.near initialize_beefy_light_client '{"initial_public_keys": ["0x02ccfa1aef0c9f012f055e216ae7546556cadd09896215edbf837b07cb6362b1f8","0x0338478804f8f14bcaab2b7c3ec85481ac4c59994ff5f92125901465b6b7c177e0","0x02c55dee371aa4ad6c2eb7b030354d38e2be227f8f221c440a83b2b0a71d42f8f9","0x03e7446dcfb7d23bbb4a98a7fe93ea9e4be5ae1734e3d7d96645c9432db5dedb37"]}' --accountId octopus-registry.near
