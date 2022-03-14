2327e45fba3c181dfe8d4b9ddb59e5bfbf36c655
Rivers Yang <rivers.yang@icloud.com>
2022-03-14 11:11:29 +0800

    Support transferring OCT token from NEAR to appchain.

--- appchain-anchor/tests/test_anchor_actions.rs
+++ appchain-anchor/tests/test_anchor_actions.rs
@@ -7,7 +7,7 @@ use mock_appchain_registry::MockAppchainRegistryContract;
 use mock_oct_token::MockOctTokenContract;
 use near_sdk::{
     json_types::{U128, U64},
-    serde_json,
+    serde_json::{self, json},
 };
 use near_sdk_sim::{call, ContractAccount, UserAccount};
 use wrapped_appchain_token::WrappedAppchainTokenContract;
@@ -887,4 +887,38 @@ fn test_equivocation_challenge() {
     } else {
         panic!("Wrong testing data for equivocation challenge.");
     }
+}
+
+#[test]
+fn test_transfer_oct_to_appchain() {
+    //
+    let (root, oct_token, _, _, anchor, users, _) = test_normal_actions(false, false);
+    //
+    let execution_result = owner_actions::register_near_fungible_token(
+        &root,
+        &anchor,
+        "OCT".to_string(),
+        "Oct token".to_string(),
+        18,
+        oct_token.account_id(),
+        U128::from(1000000),
+    );
+    assert!(execution_result.is_ok());
+    common::print_near_fungible_tokens(&anchor);
+    //
+    let execution_result = common::ft_transfer_call_oct_token(
+        &users[0],
+        &anchor.user_account,
+        common::to_oct_amount(200),
+        json!({
+            "BridgeToAppchain": {
+                "receiver_id_in_appchain": "0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d".to_string(),
+            }
+        })
+        .to_string(),
+        &oct_token,
+    );
+    assert!(execution_result.is_ok());
+    common::print_anchor_events(&anchor);
+    common::print_appchain_notifications(&anchor);
 }
