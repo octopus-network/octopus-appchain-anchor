20b0dce88cd59866f718858597d787bbb2399620
Rivers Yang <rivers.yang@icloud.com>
2022-05-11 23:27:01 +0800

    Update cli samples.

--- scripts/cli-samples/deploy_to_mainnet.sh
+++ scripts/cli-samples/deploy_to_mainnet.sh
@@ -14,7 +14,7 @@ ARGS='{"appchain_id":"'$APPCHAIN_ID'","appchain_registry":"'$REGISTRY_ACCOUNT_ID
 near call $ANCHOR_ACCOUNT_ID new $ARGS --accountId $ANCHOR_ACCOUNT_ID --gas 200000000000000
 #
 ARGS='{"account_id":"'$ANCHOR_ACCOUNT_ID'","registration_only":null}'
-near call f5cfbc74057c610c8ef151a439252680ac68c6dc.factory.bridge.near storage_deposit $ARGS --accountId $ANCHOR_ACCOUNT_ID --deposit 1
+near call f5cfbc74057c610c8ef151a439252680ac68c6dc.factory.bridge.near storage_deposit $ARGS --accountId $ANCHOR_ACCOUNT_ID --deposit 0.00125
 #
 near call $ANCHOR_ACCOUNT_ID migrate_state '' --accountId $ANCHOR_ACCOUNT_ID --gas 200000000000000
 #
--- scripts/cli-samples/deploy_to_testnet.sh
+++ scripts/cli-samples/deploy_to_testnet.sh
@@ -6,12 +6,16 @@ export APPCHAIN_ID=xxxx
 export REGISTRY_ACCOUNT_ID=registry.test_oct.testnet
 export ANCHOR_ACCOUNT_ID=$APPCHAIN_ID'.'$REGISTRY_ACCOUNT_ID
 #
-export ANCHOR_ACCOUNT_ID=barnacle-evm.registry.test_oct.testnet
 #
-near call easydeal.registry.test_oct.testnet new '{"appchain_id":"easydeal","appchain_registry":"registry.test_oct.testnet","oct_token":"oct.beta_oct_relay.testnet"}' --accountId registry.test_oct.testnet --gas 200000000000000
-near call oct.beta_oct_relay.testnet storage_deposit '{"account_id":"easydeal.registry.test_oct.testnet","registration_only":null}' --accountId dev-oct-registry.testnet --deposit 1
 #
 near deploy --accountId $ANCHOR_ACCOUNT_ID --wasmFile res/appchain_anchor.wasm
+#
+ARGS='{"appchain_id":"'$APPCHAIN_ID'","appchain_registry":"'$REGISTRY_ACCOUNT_ID'","oct_token":"oct.beta_oct_relay.testnet"}'
+near call $ANCHOR_ACCOUNT_ID new $ARGS --accountId $ANCHOR_ACCOUNT_ID --gas 200000000000000
+#
+ARGS='{"account_id":"'$ANCHOR_ACCOUNT_ID'","registration_only":null}'
+near call oct.beta_oct_relay.testnet storage_deposit $ARGS --accountId $ANCHOR_ACCOUNT_ID --deposit 0.00125
+#
 near call $ANCHOR_ACCOUNT_ID migrate_state '' --accountId $ANCHOR_ACCOUNT_ID --gas 200000000000000
 #
 near view $ANCHOR_ACCOUNT_ID get_anchor_version
