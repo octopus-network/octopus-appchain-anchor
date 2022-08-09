0b0b92a2c4c107e58d69c098da74a0907cd1e30b
Rivers Yang <rivers.yang@icloud.com>
2022-08-09 11:11:04 +0800

    Update 2.1.0-2.2.0_testnet.sh

--- scripts/cli-samples/2.1.0-2.2.0_testnet.sh
+++ scripts/cli-samples/2.1.0-2.2.0_testnet.sh
@@ -3,7 +3,7 @@
 set -e
 #
 export NEAR_ENV=testnet
-export APPCHAIN_ID=barnacle-evm
+export APPCHAIN_ID=$1
 export REGISTRY_ACCOUNT_ID=registry.test_oct.testnet
 export ANCHOR_ACCOUNT_ID=$APPCHAIN_ID'.'$REGISTRY_ACCOUNT_ID
 #
@@ -11,6 +11,6 @@ near deploy --accountId $ANCHOR_ACCOUNT_ID --wasmFile res/appchain_anchor.wasm
 #
 near call $ANCHOR_ACCOUNT_ID migrate_state '' --accountId $ANCHOR_ACCOUNT_ID --gas 200000000000000
 #
-near call $ANCHOR_ACCOUNT_ID migrate_staking_histories '{"start_index":"0"}' --accountId $ANCHOR_ACCOUNT_ID
-near call $ANCHOR_ACCOUNT_ID migrate_appchain_notification_histories '{"start_index":"0"}' --accountId $ANCHOR_ACCOUNT_ID
-near call $ANCHOR_ACCOUNT_ID migrate_appchain_messages '{"start_nonce":0}' --accountId $ANCHOR_ACCOUNT_ID
+near call $ANCHOR_ACCOUNT_ID migrate_staking_histories '{"start_index":"0"}' --accountId $ANCHOR_ACCOUNT_ID --gas 200000000000000
+near call $ANCHOR_ACCOUNT_ID migrate_appchain_notification_histories '{"start_index":"0"}' --accountId $ANCHOR_ACCOUNT_ID --gas 200000000000000
+near call $ANCHOR_ACCOUNT_ID migrate_appchain_messages '{"start_nonce":0}' --accountId $ANCHOR_ACCOUNT_ID --gas 200000000000000
